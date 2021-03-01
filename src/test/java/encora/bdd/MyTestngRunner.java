39c8aa6b472fac877dbc34eb7086da4d2676e7ea
rekhahasija <hr.hasija@gmail.com>
2021-03-01 14:06:44 +0530

    BDD Started

---
+++ src/test/java/encora/bdd/MyTestngRunner.java
@@ -0,0 +1,17 @@
+package encora.bdd;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.testng.AbstractTestNGCucumberTests;
+
+@CucumberOptions(
+        features = "src/test/resources/encora.bdd",
+        glue = {"encora.bdd"},
+        plugin = {
+                "html:target/cucumber-reports/cucumber-pretty",
+                "json:target/cucumber-reports/CucumberTestReport.json",
+                "rerun:target/cucumber-reports/rerun.txt"
+        })
+        
+public class MyTestngRunner extends AbstractTestNGCucumberTests{
+
+}
